c365664d89e97479622fb97a421e2e7020700d7e
PorvaniVafo <PorvaniVafo>
2024-03-25 18:55:39 +0600

    thrird comit

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -2,20 +2,37 @@ package controller;
 
 import entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import repository.StudentRepository;
 
 import java.util.List;
 
 @RestController
-public class StudentController {
+public class StudentControler {
 
     //localhost:8080/students
     @Autowired
     StudentRepository repo;
     @GetMapping("/students")
-    public List<Student> getAllStudents() {
+    public List<Student> getAllStudents(){
         List<Student> students = repo.findAll();
         return students;
     }
+    //localhost:8080/students/1
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable int id){
+        Student student  = repo.findById(id).get();
+
+        return student;
+    }
+
+    @PostMapping("/student/add")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void createsStudent(@RequestBody Student student) {
+        repo.save(student);
+    }
+
+
+
 }
